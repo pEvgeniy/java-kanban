92a5d4db0d500262131ce83232d72600c84673ad
pEvgeniy <PopEvgeniyV@yandex.ru>
2023-02-13 14:22:31 +0300

    feat: переопределен equals и hashCode, добавлен новый конструктор с id для возможности обновлять

---
+++ src/model/Task.java
@@ -0,0 +1,68 @@
+package model;
+
+import java.util.Objects;
+
+public class Task {
+    private final String name;
+    private final String description;
+    private int id;
+    private final TaskStatus taskStatus;
+
+    public Task(String name, String description, TaskStatus taskStatus) {
+        this.name = name;
+        this.description = description;
+        this.taskStatus = taskStatus;
+    }
+
+    public Task(String name, String description, TaskStatus taskStatus, int id) {
+        this.name = name;
+        this.description = description;
+        this.taskStatus = taskStatus;
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public TaskStatus getTaskStatus() {
+        return taskStatus;
+    }
+
+    @Override
+    public String toString() {
+        return "Task{" +
+                "name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", id=" + id +
+                ", taskStatus=" + taskStatus +
+                "}";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Task task = (Task) o;
+        return id == task.id && Objects.equals(name, task.name) &&
+                Objects.equals(description, task.description) &&
+                taskStatus == task.taskStatus;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, description, id, taskStatus);
+    }
+}
