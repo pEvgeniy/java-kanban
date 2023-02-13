7d58611467c75ac94e4050b9cb864d3b22012104
pEvgeniy <PopEvgeniyV@yandex.ru>
2023-02-13 14:28:12 +0300

    refactor: методы, проверяющие объект на класс выделены в отдельные feat: добавлены недостающие методы для Subtask'ов fix: updateTask теперь меняет не только на DONE, но и на другие статусы; изменение статуса происходит через создание нового объекта

---
+++ src/service/TaskManager.java
@@ -0,0 +1,123 @@
+package service;
+
+import model.Epic;
+import model.Subtask;
+import model.Task;
+import model.TaskStatus;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class TaskManager {
+    private final HashMap<Integer, Task> tasks = new HashMap<>();
+    private int uniqueId = 0;
+
+    public HashMap<Integer, Task> getTasks() {
+        return tasks;
+    }
+
+    public Task getTask(Integer id) {
+        if (checkForTaskClass(id) || checkForEpicClass(id)) {
+            return tasks.get(id);
+        } else {
+            throw new RuntimeException("Wrong type of task (should be Task.Class or Epic.Class)");
+        }
+    }
+
+    public ArrayList<Subtask> getEpicSubtasks(Integer epicId) {
+        if (checkForEpicClass(epicId)) {
+            return ((Epic) tasks.get(epicId)).getSubtasks();
+        } else {
+            throw new RuntimeException("Wrong epicId of task (should be id of Epic.Class)");
+        }
+    }
+
+    public void clearTasks() {
+        System.out.println("\nЗадачи удалены.");
+        tasks.clear();
+    }
+
+    public void addTask(Task task) {
+        int id = uniqueId++;
+        if (checkForEpicClass(task)) {
+            ((Epic) task).setIdToSubtasks();
+        }
+        task.setId(id);
+        System.out.println("\nЗадача " + task.getName() + " добавлена.");
+        tasks.put(id, task);
+    }
+
+    private void updateTask(Integer id, Task task) {
+        System.out.println("\nЗадача " + task.getName() + " обновлена.");
+        tasks.put(id, task);
+    }
+
+    private void updateEpic(Integer epicId, Epic epic) {
+        tasks.put(epicId, epic);
+        System.out.println("\nЗадача " + epic.getName() + " обновлена.");
+        if (epic.isNew()) {
+            tasks.put(epicId , new Epic(epic.getName(), epic.getDescription(), TaskStatus.NEW, epic.getSubtasks()));
+            System.out.println("\nСтатус задачи " + epic.getName() + " обновлен на NEW.");
+        } else if (epic.isEnded()) {
+            tasks.put(epicId , new Epic(epic.getName(), epic.getDescription(), TaskStatus.DONE, epic.getSubtasks()));
+            System.out.println("\nСтатус задачи " + epic.getName() + " обновлен на DONE.");
+        } else if (epic.isInProgress()) {
+            tasks.put(epicId , new Epic(epic.getName(), epic.getDescription(), TaskStatus.IN_PROGRESS, epic.getSubtasks()));
+            System.out.println("\nСтатус задачи " + epic.getName() + " обновлен на IN_PROGRESS.");
+        }
+    }
+
+    public void removeTask(Integer id) {
+        if (tasks.remove(id) != null) {
+            System.out.println("\nЗадача удалена.");
+        } else {
+            throw new RuntimeException("No such element in Task Map");
+        }
+    }
+
+    public void removeSubtask(Integer epicId, Integer subtaskId) {
+        ArrayList<Subtask> subtaskList = ((Epic) tasks.get(epicId)).getSubtasks();
+        Subtask subtaskToBeDeleted = subtaskList.get(subtaskId);
+        if (subtaskList.remove(subtaskToBeDeleted)) {
+            System.out.println("\nЗадача удалена.");
+            Epic epic = (Epic) tasks.get(epicId);
+            updateEpic(epicId, epic);
+        } else {
+            throw new RuntimeException("No such element in Subtask List");
+        }
+    }
+
+    public void changeTaskStatus(Task task) {
+        if (checkForTaskClass(task.getId())) {
+            updateTask(task.getId(), task);
+            return;
+        }
+        throw new RuntimeException("Wrong type of Task");
+    }
+
+    public void changeEpicStatus(Epic epic) {
+        if (checkForEpicClass(epic.getId())) {
+            updateEpic(epic.getId() , epic);
+            return;
+        }
+        throw new RuntimeException("Wrong type of Task");
+    }
+
+    private boolean checkForTaskClass(Integer id) {
+        Object task = tasks.get(id);
+        return task.getClass().equals(Task.class);
+    }
+
+    private boolean checkForTaskClass(Task task) {
+        return task.getClass().equals(Task.class);
+    }
+
+    private boolean checkForEpicClass(Integer id) {
+        Object epic = tasks.get(id);
+        return epic.getClass().equals(Epic.class);
+    }
+
+    private boolean checkForEpicClass(Task task) {
+        return task.getClass().equals(Epic.class);
+    }
+}
