ee8ad7c2edcb5c85866192b4120704f900e86533
pEvgeniy <PopEvgeniyV@yandex.ru>
2023-02-14 17:48:13 +0300

    fix: убрал лишние поля и поправил конструкотры, переопределил toString, hashCode, equals fix: isNew() теперь проверяет на subtasks.isEmpty(), т.е. эпикам с пустыми подзадачами присваевается TaskStatus.NEW

--- src/model/Epic.java
+++ src/model/Epic.java
@@ -1,29 +1,25 @@
 package model;
 
 import java.util.ArrayList;
+import java.util.Objects;
 
 public class Epic extends Task{
-    private final String name;
-    private final String description;
-    private int id;
-    private final TaskStatus taskStatus;
     private final ArrayList<Subtask> subtasks;
 
     public Epic(String name, String description, TaskStatus taskStatus, ArrayList<Subtask> subtasks) {
         super(name, description, taskStatus);
         this.subtasks = subtasks;
-        this.name = name;
-        this.description = description;
-        this.taskStatus = taskStatus;
     }
 
     public Epic(String name, String description, TaskStatus taskStatus, ArrayList<Subtask> subtasks, Integer id) {
         super(name, description, taskStatus, id);
         this.subtasks = subtasks;
-        this.name = name;
-        this.description = description;
-        this.taskStatus = taskStatus;
         this.id = id;
+    }
+
+    public Epic(String name, String description, ArrayList<Subtask> subtasks) {
+        super(name, description);
+        this.subtasks = subtasks;
     }
 
     public void setIdToSubtasks() {
@@ -36,6 +32,9 @@ public class Epic extends Task{
     }
 
     public boolean isNew() {
+        if (subtasks.isEmpty()) {
+            return true;
+        }
         int isNewCounter = 0;
         for (Subtask subtask : subtasks) {
             if (subtask.getTaskStatus().equals(TaskStatus.NEW)) {
@@ -78,5 +77,19 @@ public class Epic extends Task{
                 ", taskStatus=" + taskStatus +
                 ", subtasks=" + subtasks +
                 '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        Epic epic = (Epic) o;
+        return id == epic.id && Objects.equals(subtasks, epic.subtasks);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), id, subtasks);
     }
 }
