c2ef46ee69c2de81c16afb5c630f2260897f6539
pEvgeniy <PopEvgeniyV@yandex.ru>
2023-02-14 17:51:35 +0300

    feat: добавлены toString, hashCode, equals refactor: поле epicId стало protected

--- src/model/Subtask.java
+++ src/model/Subtask.java
@@ -1,7 +1,9 @@
 package model;
 
+import java.util.Objects;
+
 public class Subtask extends Task{
-    private int epicId;
+    protected int epicId;
 
     public Subtask(String name, String description, TaskStatus taskStatus) {
         super(name, description, taskStatus);
@@ -12,5 +14,30 @@ public class Subtask extends Task{
 
     public void setEpicId(int epicId) {
         this.epicId = epicId;
+    }
+
+    @Override
+    public String toString() {
+        return "Subtask{" +
+                "epicId=" + epicId +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", id=" + id +
+                ", taskStatus=" + taskStatus +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        Subtask subtask = (Subtask) o;
+        return epicId == subtask.epicId;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), epicId);
     }
 }
