68506121fa31687fd3b0f3a281060fa29767b7a0
pEvgeniy <PopEvgeniyV@yandex.ru>
2023-02-25 12:41:47 +0300

    feat: создан новый интерфейс

---
+++ src/service/managers/TaskManager.java
@@ -0,0 +1,36 @@
+package service.managers;
+
+import model.Epic;
+import model.Subtask;
+import model.Task;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public interface TaskManager {
+
+    HashMap<Integer, Task> getTasks();
+
+    Task getTask(Integer id);
+
+    Subtask getSubtask(Integer epicId, Integer subtaskId);
+
+    ArrayList<Subtask> getEpicSubtasks(Integer epicId);
+
+    void clearTasks();
+
+    void addTask(Task task);
+
+    void updateTask(Integer id, Task task);
+
+    void updateEpic(Integer epicId, Epic epic);
+
+    void removeTask(Integer id);
+
+    void removeSubtask(Integer epicId, Integer subtaskId);
+
+    void changeTaskStatus(Task task);
+
+    void changeEpicStatus(Epic epic);
+
+}
