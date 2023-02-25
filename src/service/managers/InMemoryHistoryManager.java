5dc2f68718aeb1c5b5f1060c76d752a1f3387bf9
pEvgeniy <PopEvgeniyV@yandex.ru>
2023-02-25 12:42:03 +0300

    feat: создан новый класс

---
+++ src/service/managers/InMemoryHistoryManager.java
@@ -0,0 +1,46 @@
+package service.managers;
+
+import model.Task;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class InMemoryHistoryManager implements HistoryManager{
+    private final List<Task> historyList = new ArrayList<>(10);
+
+    @Override
+    public void add(Task task) {
+        if (historyList.size() == 10) {
+            historyList.remove(0);
+            historyList.add(0, task);
+            return;
+        }
+        historyList.add(task);
+    }
+
+    @Override
+    public List<Task> getHistory() {
+        return historyList;
+    }
+
+    @Override
+    public String toString() {
+        return "InMemoryHistoryManager{" +
+                "historyList=" + historyList +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        InMemoryHistoryManager that = (InMemoryHistoryManager) o;
+        return Objects.equals(historyList, that.historyList);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(historyList);
+    }
+}
