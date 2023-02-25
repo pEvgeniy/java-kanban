e2f7e767a8f44d14a326b6f3775c2482c4c218a3
pEvgeniy <PopEvgeniyV@yandex.ru>
2023-02-25 13:35:10 +0300

    feat: new static method getDefaultHistory()

--- src/service/Managers.java
+++ src/service/Managers.java
@@ -1,10 +1,17 @@
 package service;
 
+import service.managers.HistoryManager;
+import service.managers.InMemoryHistoryManager;
 import service.managers.InMemoryTaskManager;
+import service.managers.TaskManager;
 
 public class Managers {
 
-    public InMemoryTaskManager getDefault() {
+    public TaskManager getDefault() {
         return new InMemoryTaskManager();
+    }
+
+    public static HistoryManager getDefaultHistory() {
+        return new InMemoryHistoryManager();
     }
 }
