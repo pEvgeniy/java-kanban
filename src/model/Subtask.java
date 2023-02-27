4e30d32c7a68df4382c670d39ef0141ff4f4e332
pEvgeniy <PopEvgeniyV@yandex.ru>
2023-02-27 12:46:20 +0300

    style

--- src/model/Subtask.java
+++ src/model/Subtask.java
@@ -8,6 +8,7 @@ public class Subtask extends Task{
     public Subtask(String name, String description, TaskStatus taskStatus) {
         super(name, description, taskStatus);
     }
+
     public int getEpicId() {
         return epicId;
     }
